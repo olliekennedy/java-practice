05aebbf34c39823bf30b3281500dd7fa5d4e6269
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-25 13:55:49 +0000

    add default capacity

--- Airport/src/airport/Airport.java
+++ Airport/src/airport/Airport.java
@@ -3,17 +3,29 @@ package airport;
 import java.util.ArrayList;
 
 public class Airport {
+    private static int airportCapacity;
+    private static String airportName;
 
-    static ArrayList hangar = new ArrayList(1);
-
-    public static void land(Plane plane) {
-        hangar.add(plane);
+    public Airport(Integer capacity, String name) {
+        airportCapacity = capacity;
+        airportName = name;
     }
 
+    static ArrayList<Plane> hangar = new ArrayList<>(airportCapacity);
+
+    public static void land(Plane plane) {
+        if (hangar.size() < airportCapacity) {
+            hangar.add(plane);
+        }
+    }
 
     public static String takeoff(Plane plane) {
         hangar.remove(plane);
         return "I, " + plane.getName() + " have taken off.";
+    }
+
+    public static String getAirportName() {
+        return airportName;
     }
 }
 
--- Airport/test/airport/AirportTest.java
+++ Airport/test/airport/AirportTest.java
@@ -7,16 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class AirportTest {
     @Test
     public void land() {
+        Airport heathrow = new Airport(1, "Heathrow");
         Plane plane = new Plane("Planey");
-        Airport.land(plane);
+        heathrow.land(plane);
         assertEquals(1, Airport.hangar.size());
         assertEquals(plane, Airport.hangar.get(0));
     }
-
-    public void takeoff() {
+    @Test
+    public void planeTakesOffAndConfirmsItIsNoLongerAtTheAirport() {
         Plane plane = new Plane("Planey");
         Airport.land(plane);
         assertEquals("I, " + plane.getName() + " have taken off.", Airport.takeoff(plane));
         assertTrue(Airport.hangar.isEmpty());
+    }
+    @Test
+    public void preventLandingAirportFull() {
+        Plane plane1 = new Plane("Plane1");
+        Airport.land(plane1);
+        Plane plane2 = new Plane("Plane2");
+        Airport.land(plane2);
+        assertFalse(Airport.hangar.contains(plane2));
+    }
+    @Test
+    public void preventLandingAirportFullWith2Capacity() {
+        Airport heathrow = new Airport(2, "Heathrow");
+        Plane plane1 = new Plane("Plane1");
+        Airport.land(plane1);
+        Plane plane2 = new Plane("Plane2");
+        Airport.land(plane2);
+        Plane plane3 = new Plane("Plane3");
+        Airport.land(plane3);
+        assertFalse(Airport.hangar.contains(plane3));
     }
 }
