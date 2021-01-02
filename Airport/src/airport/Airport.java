8b15f4e83f3e7a80cfeec0867503ed0bcd8f1e31
olliekennedy <ollie.kennedy1@gmail.com>
2021-01-02 22:43:54 +0000

    modify airport.java

--- Airport/src/airport/Airport.java
+++ Airport/src/airport/Airport.java
@@ -3,28 +3,26 @@ package airport;
 import java.util.ArrayList;
 
 public class Airport {
-    private static int airportCapacity;
-    private static String airportName;
+    private int airportCapacity;
+    private final String airportName;
 
     public Airport(Integer capacity, String name) {
         airportCapacity = capacity;
         airportName = name;
     }
 
-    static ArrayList<Plane> hangar = new ArrayList<>(airportCapacity);
+    ArrayList<Plane> hangar = new ArrayList<>(airportCapacity);
 
-    public static void land(Plane plane) {
-        if (hangar.size() < airportCapacity) {
-            hangar.add(plane);
-        }
+    public void land(Plane plane) {
+        if (hangar.size() < airportCapacity) hangar.add(plane);
     }
 
-    public static String takeoff(Plane plane) {
+    public String takeoff(Plane plane) {
         hangar.remove(plane);
         return "I, " + plane.getName() + " have taken off.";
     }
 
-    public static String getAirportName() {
+    public String getAirportName() {
         return airportName;
     }
 }
